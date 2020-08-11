d6423a8b72f807e8e53db1ef7aa5b1e3683b5fcd
Giuliana Siles <GiulianaEliDLV@gamil.com>
2020-08-11 19:28:18 -0300

    Se agregan elementos al page del Home y se agrega nueva Page para pagina Accidentes Personales

---
+++ FwkCucumber/src/main/java/pageObjects/AccidentesPersPage.java
@@ -0,0 +1,82 @@
+package pageObjects;
+
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
+
+public class AccidentesPersPage {
+
+	public AccidentesPersPage (WebDriver driver) {
+		PageFactory.initElements(driver,  this);
+	}
+	
+	// Definición de los elementos de la página
+	@FindBy(how=How.ID,using="actividades")
+	private WebElement selActividad;
+	@FindBy(how=How.ID,using="cantidad")
+	private WebElement selCantidadPers;
+	@FindBy(how=How.ID,using="periodo")
+	private WebElement selPeriodo;
+	@FindBy(how=How.ID,using="desde")
+	private WebElement tbxFechaDesde;
+	@FindBy(how=How.ID,using="hasta")
+	private WebElement tbxFechaHasta;
+	@FindBy(how=How.ID,using="ap-nombre")
+	private WebElement tbxNombre;
+	@FindBy(how=How.ID,using="ap-apellido")
+	private WebElement tbxApellido;
+	@FindBy(how=How.ID,using="ap-email")
+	private WebElement tbxEmail;
+	@FindBy(how=How.ID,using="ap-prefijo")
+	private WebElement tbxPrefijo;
+	@FindBy(how=How.ID,using="ap-telefono")
+	private WebElement tbxTelefono;
+	@FindBy(how=How.ID,using="contratar-ap")
+	private WebElement btnCotiza;
+	
+	// Definición de las acciones sobre los objectos
+	public void selecciona_Actividad(String actividad) {
+		Select dpdActividad = new Select (selActividad);
+		dpdActividad.selectByVisibleText(actividad);
+	}
+	public void selecciona_CantidadPersonas(String cantidad) {
+		Select dpdCantidadPers = new Select (selCantidadPers);
+		dpdCantidadPers.selectByVisibleText(cantidad);
+	}
+	public void selecciona_periodo(String periodo) {
+		Select dpdPeriodo = new Select(selPeriodo);
+		dpdPeriodo.selectByValue(periodo);
+	}
+	public void ingresa_FechaDesde(String fechaDesde) {
+		tbxFechaDesde.sendKeys(fechaDesde);
+	}
+	public void confirma_FechaDesde() {
+		tbxFechaDesde.sendKeys(Keys.ENTER);
+	}
+	public void ingresa_FechaHasta(String fechaHasta) {
+		tbxFechaHasta.sendKeys(fechaHasta);
+	}
+	public void ingresa_Nombre(String nombre) {
+		tbxNombre.sendKeys(nombre);
+	}
+	public void ingresa_Apellido(String apellido) {
+		tbxApellido.sendKeys(apellido);
+	}
+	public void ingresa_Email(String email) {
+		tbxEmail.sendKeys(email);
+	}
+	public void ingresa_Prefijo(String prefijo) {
+		tbxPrefijo.sendKeys(prefijo);
+	}
+	public void ingresa_Telefono(String telefono) {
+		tbxTelefono.sendKeys(telefono);
+	}
+	public void cliquea_BotonCotiza() {
+		btnCotiza.click();
+	}
+	
+}
--- FwkCucumber/src/main/java/pageObjects/ExpertaHomePage.java
+++ FwkCucumber/src/main/java/pageObjects/ExpertaHomePage.java
@@ -1,5 +1,6 @@
 package pageObjects;
 
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -12,18 +13,36 @@ public class ExpertaHomePage {
 		PageFactory.initElements(driver,  this);
 	}
 	
-	//
+	// Definición de los elementos de la página
 	@FindBy(how=How.XPATH,using="//div[@id='desktopMenu']/div[1]/p")
 	private WebElement btnSeguros;
 	@FindBy(how=How.XPATH,using="//li[@id='menu-item-4673']/a")
 	private WebElement btnAutos;
+	@FindBy(how=How.ID,using="searchButton")
+	private WebElement btnLupa;
+	@FindBy(how=How.ID,using="search-text-header")
+	private WebElement tbxBuscar;
+	@FindBy(how=How.XPATH,using="//a[contains(@href,'cotizar-seguros-accidentes-personales')]")
+	private WebElement lnkCotizaAccPers;
 	
-	//
+	// Definición de las acciones sobre los objectos
 	public void cliquea_MenuSeguros() {
 		btnSeguros.click();
 	}
 	public void cliquea_OpcionAutos() {
 		btnAutos.click();
+	}
+	public void cliquea_BotonLupa() {
+		btnLupa.click();
+	}
+	public void ingresa_TextoABuscar(String texto) {
+		tbxBuscar.sendKeys(texto);
+	}
+	public void confirma_TextoABuscar() {
+		tbxBuscar.sendKeys(Keys.ENTER);
+	}
+	public String selecciona_URLCotizarAccidentePers() {
+		return lnkCotizaAccPers.getAttribute("href");
 	}
 	
 }
